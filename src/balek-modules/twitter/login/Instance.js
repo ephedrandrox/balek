7266154b4ae01940eb7cad1cc821125e390ccbdf
Blake Glanville <blake@digivigil.com>
2020-09-25 18:04:20 -0700

    Extended login component in twitter/login module

--- src/balek-modules/twitter/login/Instance.js
+++ src/balek-modules/twitter/login/Instance.js
@@ -1,9 +1,9 @@
 //This file is loaded by the module when it is loaded into the Balek Instance
 //And is used to create a new instance when it is requested by the moduleManager
 define(['dojo/_base/declare',
-        'balek-modules/Instance',],                                     //Array of files to include
-    function (declare, baseInstance) {                                  //variables from array of included files
-        return declare("moduleTwitterLoginInstance", baseInstance, {    //Declares instance extending base instance
+        'balek-modules/components/login/Instance',],                                     //Array of files to include
+    function (declare, _loginInstance) {                                  //variables from array of included files
+        return declare("moduleTwitterLoginInstance", _loginInstance, {    //Declares instance extending base instance
             _instanceKey: null,                                         //used to identify instance, set by moduleManager
 
             constructor: function (args) {                              //called when a new instance is created by moduleManager for a session
--- src/balek-modules/twitter/login/Module.js
+++ src/balek-modules/twitter/login/Module.js
@@ -5,12 +5,12 @@ define(['dojo/_base/declare',
         'balek-modules/twitter/login/Instance',],               //array of files to include
     function (declare, baseModule, moduleInstance) {            //variables from array of included files
         return declare("moduleTwitterLoginModule", baseModule, {   //Declares module extending base module
-            _displayName: "Twitter Dev Login",                  //This is the name used for user interaction
+            _displayName: "Twitter Login",                  //This is the name used for user interaction
             _allowedSessions: [0],                              //Array of Status Sessions are allowed to have that load this module
 
             constructor: function (args) {                      //Module constructor called when module is loaded into moduleManager
                 declare.safeMixin(this, args);                  //Used to mix in any arguments that need to be added by moduleManager
-                console.log("twitterDevLoginModule  starting...");
+                console.log("twitterLoginModule  starting...");
             },
             newInstance: function (args) {                      //this is what is called when a new instance is requested
                 return new moduleInstance(args);                //returns new instance of moduleInstance aka 'balek-modules/dev-twitter/login/Instance'
