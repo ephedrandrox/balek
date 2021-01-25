9e185a226f5a9abbc0753f86c269b96214dcdf20
Blake Glanville <blake@digivigil.com>
2021-01-25 12:45:51 -0800

    Updated admin/user module to work with new containers

--- src/balek-modules/admin/users/Instance/userManagement.js
+++ src/balek-modules/admin/users/Instance/userManagement.js
@@ -5,17 +5,16 @@ define(['dojo/_base/declare',
     ],
     function (declare, lang, topic, syncedCommanderInstance) {
 
-        return declare("moduleAdminUsersInstance", syncedCommanderInstance, {
+        return declare("moduleAdminUsersUserManagementInstance", syncedCommanderInstance, {
             _instanceKey: null,
 
             constructor: function (args) {
                 declare.safeMixin(this, args);
 
-                console.log("moduleAdminUsersInstance starting...");
+                console.log("moduleAdminUsersUserManagementInstance starting...");
 
-            },
-            receiveMessage: function (moduleMessage, wssConnection) {
-                console.log("shouldn't see anything here", moduleMessage);
+                this._interfaceState.set("moduleName","moduleAdminUsersUserManagementInstance");
+
             }
         });
     }
