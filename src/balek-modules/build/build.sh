3f62e16c7be53f1929c1ea11cf75c7d7003d46d9
Blake Glanville <blake@digivigil.com>
2021-02-19 15:56:56 -0800

    added quill css copy to lib build

--- src/balek-modules/build/build.sh
+++ src/balek-modules/build/build.sh
@@ -23,11 +23,15 @@ cp -Rf node_modules/codemirror lib/
 #Quill
 
 mkdir -p lib/quill
+mkdir -p lib/quill/dist
+
 
 npm install --prefix build/quill/
 npm run build --prefix build/quill/
 #copy main javascript library
-cp -Rf build/quill/dist/quill.js lib/quill/dist
+cp  build/quill/dist/quill.js lib/quill/dist/quill.js
+cp  build/quill/dist/quill.core.css lib/quill/dist/quill.core.css
+cp  build/quill/dist/quill.snow.css lib/quill/dist/quill.snow.css
 
 
 
