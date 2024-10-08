0d22003106b5f4aa4af8ab50c6eb6159aebfb5cd
Blake Glanville <blake@digivigil.com>
2024-10-07 17:38:50 -0700

    path fix

--- util/production/addDevice.sh
+++ util/production/addDevice.sh
@@ -3,4 +3,4 @@
 source "$(dirname "${BASH_SOURCE[0]}")/common_paths.sh"
 
 # run the addDevice.js script on the production container with arguments passed to this script
-docker compose -f "$DOCKER_COMPOSE_FILE"  exec -T digiscan node util/addDevice.js "$@"
+docker compose -f "$DOCKER_COMPOSE_FILE"  exec -T digiscan node util/database/addDevice.js "$@"
