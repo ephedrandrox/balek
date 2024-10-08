2f8930bfe7b2b3a21e229c71c5a0ca93e0382ed2
Blake Glanville <blake@digivigil.com>
2024-10-07 17:36:50 -0700

    execute added

---
+++ util/production/addDevice.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Include the common paths
+source "$(dirname "${BASH_SOURCE[0]}")/common_paths.sh"
+
+# run the addDevice.js script on the production container with arguments passed to this script
+docker compose -f "$DOCKER_COMPOSE_FILE"  exec -T digiscan node util/addDevice.js "$@"
